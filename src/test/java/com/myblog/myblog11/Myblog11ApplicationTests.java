89ac41f97d9fab94cbafe46a754af1bd08425743
Bhushan Yevatkar <by9304@gmail.com>
2024-01-16 21:31:13 +0530

    127:Create save Post Feature Entity class added 4th Jan 2024

--- src/test/java/com/myblog/myblog11/Myblog11ApplicationTests.java
+++ src/test/java/com/myblog/myblog11/Myblog11ApplicationTests.java
@@ -1,7 +1,10 @@
 package com.myblog.myblog11;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootTest
 class Myblog11ApplicationTests {
